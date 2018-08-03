44e5a07b281346c8d1f7520dfd4fcd2f42300309
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2018-08-03 12:28:26 +0800

    Apply UI feature to the game
    This update applies the basic UI for the game. This update also includes the basic script files for the mecha and enemy.

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class Enemy{
+
+    private int health;
+    private int damage;
+
+
+    public Enemy()
+    {
+        this.health = 200;
+        this.damage = 20;
+    }
+
+    public void takeDamage(Player hero)
+    {
+        this.health -= hero.getDamage();
+    }
+
+    public int getDamage()
+    {
+        return this.damage;
+    }
+
+    public int getHealth()
+    {
+        return this.health;
+    }
+}
