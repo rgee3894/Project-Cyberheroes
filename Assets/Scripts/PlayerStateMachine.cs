5d38760e33b9f35e03abdab64893ebfb66a42c07
Marl Christian Ricanor <32050672+Marlcelo@users.noreply.github.com>
2018-08-12 19:07:09 +0800

    Added more UI Elements
    Added the Player Clock, as well as the "battery" slider

--- Assets/Scripts/PlayerStateMachine.cs
+++ Assets/Scripts/PlayerStateMachine.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 public class PlayerStateMachine : MonoBehaviour {
 
     public Player player = new Player();
+
+    public enum TurnState
+    {
+        ANSWERING,
+        WAITING,
+        ACTION,
+        DEAD
+    }
+
+    public TurnState currentState;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -12,6 +23,29 @@ public class PlayerStateMachine : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        switch (currentState)
+        {
+            case (TurnState.ANSWERING):
+
+            break;
+
+            case (TurnState.WAITING):
+
+            break;
+
+            case (TurnState.ACTION):
+
+            break;
+
+            case (TurnState.DEAD):
+
+            break;
+        }
+
 	}
+
+    void UpgradeProgressBar()
+    {
+
+    }
 }
